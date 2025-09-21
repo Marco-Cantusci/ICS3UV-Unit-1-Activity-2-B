41db5f81ecf00fc313ed19ca835875ae57b5695c
default <marco.cantusci@stu.ocsb.ca>
2025-09-21 23:19:30 +0000

    Added name and done

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-21
+ * @fileoverview Printing my name.
+ */
+
+// Print out name
+console.log("Marco Cantusci")
+
+console.log("\nDone.")
